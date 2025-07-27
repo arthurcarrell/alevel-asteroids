3c2cfb7f223614152304266fe1d2ec3169f0b03f
arthurcarrell <Arthurcarrell08@gmail.com>
2025-07-27 12:14:08 +0100

    stuff

--- obj/Debug/net8.0/alevel-asteroids.AssemblyInfo.cs
+++ obj/Debug/net8.0/alevel-asteroids.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alevel-asteroids")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20259342e35950d6891e11594f1c60b74d656f5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bc0e30fc771df665b1883add1b0876e4e2411f")]
 [assembly: System.Reflection.AssemblyProductAttribute("alevel-asteroids")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alevel-asteroids")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
