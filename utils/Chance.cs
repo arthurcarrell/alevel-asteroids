0e694fad5fd71c4beb2f356ff71e730954fcf3c5
arthurcarrell <Arthurcarrell08@gmail.com>
2025-09-01 11:31:17 +0100

    got rid of an unessasary log

--- utils/Chance.cs
+++ utils/Chance.cs
@@ -4,8 +4,6 @@ public class Chance
 {
     private static Random random = new Random();
     public static bool Percentage(float percentage, float proc_chance=1) {
-        bool answer = random.Next(1, 100 + 1) >= 100 - (percentage * proc_chance * 100);
-        Console.WriteLine($"percentage: {answer}");
-        return answer;
+        return random.Next(1, 100 + 1) >= 100 - (percentage * proc_chance * 100);
     }
 }
