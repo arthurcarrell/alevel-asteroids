7f82851c7a70ee867158c7321c0d2dff9e6d1c4b
arthurcarrell <Arthurcarrell08@gmail.com>
2025-09-04 17:20:52 +0000

    Chance.Item allows a random item from a list of objects to be picked

--- utils/Chance.cs
+++ utils/Chance.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 public class Chance
 {
     private static Random random = new Random();
     public static bool Percentage(float percentage, float proc_chance=1) {
         return random.Next(1, 100 + 1) > 100 - (percentage * proc_chance * 100);
+    }
+
+    public static T Item<T>(List<T> items) {
+        return items[random.Next(0, items.Count)];
     }
 }
