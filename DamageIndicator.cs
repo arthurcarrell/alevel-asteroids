e352db70644f104d7257f1895b975889df8f7252
arthurcarrell <Arthurcarrell08@gmail.com>
2025-09-24 13:47:40 +0000

    Size of the Damage Indicator is now dependent on the damage dealt

--- DamageIndicator.cs
+++ DamageIndicator.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -8,10 +9,18 @@ class DamageIndicator : Entity
 
     private float damageTime;
     private Damage damage;
+    private float damageRatio;
     public DamageIndicator(Damage setDamage, Vector2 setPosition, int setDamageTime = 1000, float setRotation = 0, float setScale = 1) : base(null, setPosition, setRotation, setScale)
     {
         damageTime = setDamageTime;
         damage = setDamage;
+
+        /* get the damage ratio (how much damage is this above that entities base damage)
+        if an entity has 20 damage and deals 20 damage then thats a ratio of 1
+        if an entity has 20 damage and deals 200 damage then thats a ratio of 10
+        for the sake of the text looking nice the lowest this number can go is 1 and the highest is 5
+        */
+        damageRatio = Math.Min(5, Math.Max(1, 1 + damage.amount / damage.source.GetDamage()/4));
     }
 
     public override void Render(SpriteBatch spriteBatch)
@@ -23,7 +32,11 @@ class DamageIndicator : Entity
             _ => Color.Yellow
         };
 
-        TextDrawer.DrawText(spriteBatch, new Text($"{damage.amount}", damageColor), position, Textures.galaticSpaceFont);
+        
+
+        
+
+        TextDrawer.DrawText(spriteBatch, new Text($"{damage.amount}", damageColor), position, Textures.galaticSpaceFont, damageRatio);
     }
 
     public override void Update(GameTime gameTime)
